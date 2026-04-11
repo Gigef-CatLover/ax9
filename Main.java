4746226a495c103b4d7f469b4dabcdc4acfdce8a
MrYeeet <175942030+Gigef-CatLover@users.noreply.github.com>
2026-04-11 13:40:51 +0200

    nothing works

--- Main.java
+++ Main.java
@@ -11,32 +11,45 @@ import java.util.*;
 import java.io.*;
 
 
+
 public class Main{
   public static void main(String[] args){
         String Encrpath = "text.txt";
-        String Alf = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String alf = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String[] hex = {"41","42","43","44","45","46","46","47","48", "49" , "4A" , "4B" , "4C" , "4D" , "4E" , "4F" , "50", "51" , "52" , "53" , "54" , "55" , "56" , "57" , "58" , "59" , "5A"};
+
         File file = new File(Encrpath);
 
         try(Scanner myreader = new Scanner(file)){
-
-          while(myreader.hasNext()){
+            while(myreader.hasNext()){
 
             String frase = myreader.nextLine();
             String[] parola = frase.split(" ");
 
-            System.out.println(Alf.charAt(2));
-            System.out.println(frase.charAt(2));
+            // System.out.println(Alf.length());
+            ArrayList<String> lettere = new ArrayList<String>();
+            String prova = "prova";
 
-            for(int i = 0 ; i < parola.length ; i++){
-
-              System.out.println(parola[i] + " " + i + " " + parola.length);
+            for(int i = 0 ; i < prova.length() ;  i++){
+              lettere.add(i , Character.toString(prova.charAt(i)));
+            }
+            System.out.println(prova.charAt(0) == alf.charAt(0));
+            /**
+            for(int i = 0 ; i < prova.length() ; i++){
+                for (int j = 0 ; j < alf.length() ; j++ ){
+                    if(prova.charAt(i) == alf.charAt(j)){
+                        // lettere.set(i , hex[j]);
+                        System.out.println("Si!" + prova.charAt(i) + " " + alf.charAt(j));
+                    }
+                }
             }
 
+            System.out.println(lettere);
+            **/
           }
+        }catch(Exception e){
+            System.out.println("vabbeh");
 
-      }catch(Exception e){
-          System.out.println("vabbeh");
-
-      }
+        }
   }
 }
